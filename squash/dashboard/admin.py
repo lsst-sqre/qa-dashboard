ac6473d61e755074e497361feb1dad4a9c18d99b
Angelo Fausti <afausti@lsst.org>
2016-04-07 23:26:58 -0700

    Registered models to see them in Django admin interface

--- squash/dashboard/admin.py
+++ squash/dashboard/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Dataset, Visit, Ccd
+    
+admin.site.register(Dataset)
+admin.site.register(Visit)
+admin.site.register(Ccd)
 
-# Register your models here.
