c28ee019361acf42f0a656d56f40b3d44621cbd5
Angelo Fausti <afausti@lsst.org>
2016-04-07 23:35:01 -0700

    Added template for generating static pages layout

--- squash/squash/urls.py
+++ squash/squash/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    	url(r'^admin/', include(admin.site.urls)),
+	url(r'^(?P<name>[\w./-]+)/$', views.show_page, name='page'),
+	url(r'^$', views.show_page),
 ]
