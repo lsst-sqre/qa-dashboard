e6b045847ded94ed981d9a330f2d1e382be15869
Angelo Fausti <afausti@lsst.org>
2016-05-03 22:30:52 -0700

    Added time series plot for monitoring kpm

---
+++ squash/dashboard/viz/plots.py
@@ -0,0 +1,42 @@
+from math import pi
+from bokeh.plotting import Figure
+from bokeh.models import Legend
+from bokeh.models import DatetimeTickFormatter
+
+
+def time_series_defaults(hover):
+    """
+    Defaults for time series plot
+    """
+    plot = Figure(
+        plot_height=400,
+        plot_width=900,
+        tools="xpan,xwheel_zoom,xbox_zoom,reset",
+        x_axis_type='datetime',
+        min_border_top=0,
+        min_border_right=10,
+        min_border_bottom=50,
+        min_border_left=50,
+        outline_line_color=None,
+    )
+    plot.add_tools(hover)
+    plot.x_range.follow = "end"
+    plot.x_range.range_padding = 0
+    plot.xaxis.axis_label = "Runtime"
+    plot.xaxis.major_label_orientation = pi/4
+    plot.xaxis.formatter = DatetimeTickFormatter(formats=dict(
+        hours=["%d %B"],
+        days=["%d %B"],
+        months=["%d %B"],
+        years=["%d %B"],
+    ))
+    return plot
+
+
+def legend_defaults(legends):
+    return Legend(
+        legends=legends,
+        location='top_right',
+        border_line_color=None,
+        background_fill_alpha=0.7
+    )
