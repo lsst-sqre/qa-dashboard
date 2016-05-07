658056885daa66c6eea0900734e1518971dc3447
Jonathan Sick <jsick@lsst.org>
2016-05-06 17:16:31 -0700

    Add VersionedPackage model to track Git versions
    We want to track the version of the Stack used to generate each job.
    EUPS makes this a little tricky because there's no portable version
    database that can be queried.
    
    The VersionedPackage table attempts to capture what versions of Stack
    software were used during a QA test run. Each Job has many
    VersionedPackages that it references (one-to-many).
    
    VersionedPackages are serialized within a Job resources under the
    'packages' field.
    
    Note that originally I attempted to re-factor the VersionedPackage table
    into both a Package table and a Package Version table.  The Package
    model has records for every EUPS package in a stack. The PackageVersion
    has records for the version of a package seen in a job.  Thus if there
    are N packages, there will be N rows in the PackageVersion table for
    every build.  This was implemented as a many-to-many relationship
    between Jobs and Packages with the PackageVersion table as an
    intermediary.  Unfortunately it was difficult (impossible?) to make this
    work with the Django REST Framework as a nested serialization.

--- squash/dashboard/models.py
+++ squash/dashboard/models.py
@@ -1,3 +1,4 @@
+import json
 from django.db import models
 from django.contrib.auth.models import User
 from .bokeh_utils import update_bokeh_sessions
@@ -23,6 +24,35 @@ class Job(models.Model):
 
     def __str__(self):
         return self.build
+
+
+class VersionedPackage(models.Model):
+    """A specific version of an Eups Product used in a Job."""
+    name = models.SlugField(
+        max_length=64, null=False,
+        help_text='EUPS package name')
+    git_url = models.URLField(
+        max_length=128,
+        help_text='Git repo URL for package')
+    git_commit = models.CharField(
+        max_length=40, null=False,
+        help_text='SHA1 hash of the git commit')
+    git_branch = models.TextField(
+        help_text='Resolved git branch that the commit resides on')
+    build_version = models.TextField(
+        help_text='EUPS build version')
+
+    job = models.ForeignKey(Job, null=False, related_name='packages')
+
+    def __str__(self):
+        return json.dumps({'_class': 'VersionedPackage',
+                           'job.build': self.job.build,
+                           'name': self.name,
+                           'git_url': self.git_url,
+                           'git_commit': self.git_commit,
+                           'git_branch': self.git_branch,
+                           'build_version': self.build_version},
+                          indent=2, sort_keys=True)
 
 
 class Metric(models.Model):
