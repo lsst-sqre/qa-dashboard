9c35a41c6594d0ac482a558abf4772150c2a67e9
Jonathan Sick <jsick@lsst.org>
2016-05-11 10:35:18 -0700

    Make all API resource collection endpoints plural
    /api/job/ -> /api/jobs/
    /api/metric/ -> /api/metrics/
    
    It's a standard convention in RESTful APIs to make endpoints for
    collections plural.

--- squash/dashboard/urls.py
+++ squash/dashboard/urls.py
@@ -4,9 +4,16 @@ from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.routers import DefaultRouter
 from . import views
 
+<<<<<<< HEAD
 router = DefaultRouter()
 router.register(r'job', views.JobViewSet)
 router.register(r'metric', views.MetricViewSet)
+=======
+api_router = DefaultRouter()
+api_router.register(r'jobs', views.JobViewSet)
+api_router.register(r'metrics', views.MetricViewSet)
+api_router.register(r'packages', views.PackageViewSet)
+>>>>>>> b962845... Make all API resource collection endpoints plural
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
