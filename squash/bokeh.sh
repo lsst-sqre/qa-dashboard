bfd842c94c19840291ffbd7cdd7ae9b83bd03646
Joshua Hoblitt <josh@hoblitt.com>
2016-05-11 13:14:33 -0700

    fix bokeh server listening port syntax

--- squash/bokeh.sh
+++ squash/bokeh.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-bokeh serve --log-level=info --host=localhost:5006 --host=localhost:8000
+bokeh serve --log-level=info --port=5006 --host=127.0.0.1:5006 --host=localhost:5006 --host=127.0.0.1:8000 --host=localhost:8000
