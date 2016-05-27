71403268c270c2e2eb96268d1bb6c005b3fd981b
Angelo Fausti <afausti@lsst.org>
2016-05-27 14:59:03 -0700

    Added VersionedPackage model to django admin interface

--- squash/dashboard/admin.py
+++ squash/dashboard/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Job, Metric, Measurement, UserSession
+from .models import Job, Metric, Measurement, VersionedPackage, UserSession
 
 admin.site.register(Job)
 admin.site.register(Metric)
 admin.site.register(Measurement)
+admin.site.register(VersionedPackage)
 admin.site.register(UserSession)
