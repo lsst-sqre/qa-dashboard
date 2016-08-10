93ee88207a4dc69c8ab6c433075f7aa51655e0ce
Angelo Fausti <afausti@lsst.org>
2016-08-10 14:38:20 -0700

    Changes in the dashboard API to decouple bokeh from django
    - added new endpoint to retrieve datasets
    - added filtering to retrieve jobs for specific datasets

--- squash/dashboard/urls.py
+++ squash/dashboard/urls.py
@@ -8,12 +8,14 @@ admin.site.site_header = 'SQUASH Admin'
 api_router = DefaultRouter()
 api_router.register(r'jobs', views.JobViewSet)
 api_router.register(r'metrics', views.MetricViewSet)
+api_router.register(r'datasets', views.DatasetViewSet, base_name='datasets')
 
 urlpatterns = [
-    url(r'^dashboard/admin', include(admin.site.urls)),
     url(r'^dashboard/api/', include(api_router.urls)),
-    url(r'^dashboard/api/token/', obtain_auth_token, name='api-token'),
+    url(r'^dashboard/admin', include(admin.site.urls)),
+    url(r'^dashboard/api/token', obtain_auth_token, name='api-token'),
     url(r'^$', views.HomeView.as_view(), name='home'),
-    url(r'^(?P<pk>[a-zA-Z0-9]*$)', views.ListMetricsView.as_view(),
-        name='metrics-list'),
+    url(r'^(?P<pk>[a-zA-Z0-9]*$)', views.MetricsView.as_view(),
+        name='metrics-list')
+
 ]
