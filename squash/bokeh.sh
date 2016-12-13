a19fcd31498dd52e3d0995723b6f32546de971c5
Angelo Fausti <afausti@lsst.org>
2016-12-13 14:23:17 -0700

    Start multiple bokeh apps

--- squash/bokeh.sh
+++ squash/bokeh.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-bokeh serve --allow-websocket-origin=localhost:8000 dashboard/viz/metrics.py
+export PYTHONPATH=$PYTHONPATH:.
+bokeh serve --allow-websocket-origin=localhost:8000 dashboard/viz/regression dashboard/viz/astrometry dashboard/viz/photometry
