a386b855e097a5f9527765db5f58c5b53d2eb9a2
Angelo Fausti <afausti@lsst.org>
2017-01-06 11:41:11 -0700

    Avoid warnings returned by ./manage.py check
    - Set MySQL and InnoDB strict mode in the db settings
    - See: https://django-mysql.readthedocs.io/en/latest/checks.html

--- squash/squash/settings/dev.py
+++ squash/squash/settings/dev.py
@@ -17,6 +17,7 @@ DATABASES = {
         'PORT': '3306',
         'OPTIONS': {
               'charset': 'utf8mb4',
+              'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
         },
         'TEST': {
             'CHARSET': 'utf8mb4',
