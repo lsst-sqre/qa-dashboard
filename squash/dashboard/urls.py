55cbfd6c006bc3404875cad7bb4781fc37c3e47b
Angelo Fausti <afausti@lsst.org>
2017-03-28 04:38:34 -0700

    Reviewed urlconf regular expressions
    - make trailing slash mandatory for the bokeh app urls
    - evaluate home page first

--- squash/dashboard/urls.py
+++ squash/dashboard/urls.py
@@ -8,17 +8,19 @@ admin.site.site_header = 'SQUASH Admin'
 api_router = DefaultRouter()
 api_router.register(r'jobs', views.JobViewSet)
 api_router.register(r'metrics', views.MetricViewSet)
-api_router.register(r'measurements', views.MeasurementViewSet,
-                    base_name='measurements')
 api_router.register(r'datasets', views.DatasetViewSet,
                     base_name='datasets')
+
+# endpoints for data consumed by the bokeh apps
+api_router.register(r'measurements', views.MeasurementViewSet,
+                    base_name='measurements')
 api_router.register(r'AMx', views.AMxViewSet, base_name='AMx')
 api_router.register(r'PAx', views.PAxViewSet, base_name='PAx')
 
 urlpatterns = [
+    url(r'^$', views.home, name='home'),
     url(r'^dashboard/api/', include(api_router.urls)),
     url(r'^dashboard/admin/', include(admin.site.urls)),
-    url(r'^dashboard/(?P<bokeh_app>[\w./-]+)/?$',
-        views.embed_bokeh, name='bokeh_app'),
-    url(r'$', views.home, name='home')
+    url(r'^dashboard/(?P<bokeh_app>[\w./-]+)/$', views.embed_bokeh,
+        name='embed_bokeh')
 ]
